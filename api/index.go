ec11327dfadc359db4770ba07cacecb8a0d7b5ca
Parnenoia <sneyers.arne@gmail.com>
2023-11-30 20:19:48 +0100

    fdgf

--- api/index.go
+++ api/index.go
@@ -2,8 +2,6 @@ package handler
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
 )
 
@@ -15,29 +13,29 @@ type Film struct {
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("hello world")
 
-	h1 := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("index.html"))
-		films := map[string][]Film{
-			"Films": {
-				{Title: "The Godfather", Director: "FFC"},
-				{Title: "Blade Runnder", Director: "Ridley Scott"},
-			},
-		}
-		tmpl.Execute(w, films)
-	}
-	http.HandleFunc("/", h1)
+	// h1 := func(w http.ResponseWriter, r *http.Request) {
+	// 	tmpl := template.Must(template.ParseFiles("index.html"))
+	// 	films := map[string][]Film{
+	// 		"Films": {
+	// 			{Title: "The Godfather", Director: "FFC"},
+	// 			{Title: "Blade Runnder", Director: "Ridley Scott"},
+	// 		},
+	// 	}
+	// 	tmpl.Execute(w, films)
+	// }
+	// http.HandleFunc("/", h1)
 
-	h2 := func(w http.ResponseWriter, r *http.Request) {
-		title := r.PostFormValue("title")
-		director := r.PostFormValue("director")
-		htmlStr := fmt.Sprintf("<h2>%s</h2><h4>%s</h4>", title, director)
-		tmpl, _ := template.New("new_movie").Parse(htmlStr)
-		tmpl.Execute(w, nil)
-		fmt.Print("Movie with title:", title, "and director:", director, "added\n")
-		log.Print("HTMX request received")
-		log.Print(r.Header.Get("HX-request"))
-	}
-	http.HandleFunc("/add-film/", h2)
+	// h2 := func(w http.ResponseWriter, r *http.Request) {
+	// 	title := r.PostFormValue("title")
+	// 	director := r.PostFormValue("director")
+	// 	htmlStr := fmt.Sprintf("<h2>%s</h2><h4>%s</h4>", title, director)
+	// 	tmpl, _ := template.New("new_movie").Parse(htmlStr)
+	// 	tmpl.Execute(w, nil)
+	// 	fmt.Print("Movie with title:", title, "and director:", director, "added\n")
+	// 	log.Print("HTMX request received")
+	// 	log.Print(r.Header.Get("HX-request"))
+	// }
+	// http.HandleFunc("/add-film/", h2)
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	// log.Fatal(http.ListenAndServe(":8000", nil))
 }
