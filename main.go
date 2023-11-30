970b9bfeec75bdcc83bc3c73054f361f8c395e0b
Parnenoia <sneyers.arne@gmail.com>
2023-11-30 18:03:48 +0100

    new upload

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	fmt.Println("hello world")
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl := template.Must(template.ParseFiles("index.html", "Untitled-1.svg"))
 		films := map[string][]Film{
 			"Films": {
 				{Title: "The Godfather", Director: "FFC"},
@@ -26,6 +26,18 @@ func main() {
 		tmpl.Execute(w, films)
 	}
 	http.HandleFunc("/", h1)
+
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+		htmlStr := fmt.Sprintf("<h2>%s</h2><h4>%s</h4>", title, director)
+		tmpl, _ := template.New("new_movie").Parse(htmlStr)
+		tmpl.Execute(w, nil)
+		fmt.Print("Movie with title:", title, "and director:", director, "added\n")
+		log.Print("HTMX request received")
+		log.Print(r.Header.Get("HX-request"))
+	}
+	http.HandleFunc("/add-film/", h2)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
