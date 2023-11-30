8b941aba053246aa635c3a166ada1b9e9c6fa08b
Parnenoia <sneyers.arne@gmail.com>
2023-11-30 15:25:42 +0100

    test

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Film struct {
+	Title    string
+	Director string
+}
+
+func main() {
+	fmt.Println("hello world")
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		films := map[string][]Film{
+			"Films": {
+				{Title: "The Godfather", Director: "FFC"},
+				{Title: "Blade Runnder", Director: "Ridley Scott"},
+			},
+		}
+		tmpl.Execute(w, films)
+	}
+	http.HandleFunc("/", h1)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
