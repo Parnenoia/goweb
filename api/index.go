c6cc6beb17a75b3046cc0da7b2fb032a71dd28cf
Parnenoia <sneyers.arne@gmail.com>
2023-12-02 23:18:45 +0100

    test 8

--- api/index.go
+++ api/index.go
@@ -12,32 +12,31 @@ type Film struct {
 	Director string
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("hello world")
-
-	h1 := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("index.html"))
-		films := map[string][]Film{
-			"Films": {
-				{Title: "The Godfather", Director: "FFC"},
-				{Title: "Blade Runnder", Director: "Ridley Scott"},
-			},
-		}
-		tmpl.Execute(w, films)
+func HandlerOne(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	films := map[string][]Film{
+		"Films": {
+			{Title: "The Godfather", Director: "FFC"},
+			{Title: "Blade Runnder", Director: "Ridley Scott"},
+		},
 	}
-	http.HandleFunc("/", h1)
+	tmpl.Execute(w, films)
+}
 
-	h2 := func(w http.ResponseWriter, r *http.Request) {
-		title := r.PostFormValue("title")
-		director := r.PostFormValue("director")
-		htmlStr := fmt.Sprintf("<h2>%s</h2><h4>%s</h4>", title, director)
-		tmpl, _ := template.New("new_movie").Parse(htmlStr)
-		tmpl.Execute(w, nil)
-		fmt.Print("Movie with title:", title, "and director:", director, "added\n")
-		log.Print("HTMX request received")
-		log.Print(r.Header.Get("HX-request"))
-	}
-	http.HandleFunc("/add-film/", h2)
+func HandlerTwo(w http.ResponseWriter, r *http.Request) {
+	title := r.PostFormValue("title")
+	director := r.PostFormValue("director")
+	htmlStr := fmt.Sprintf("<h2>%s</h2><h4>%s</h4>", title, director)
+	tmpl, _ := template.New("new_movie").Parse(htmlStr)
+	tmpl.Execute(w, nil)
+	fmt.Print("Movie with title:", title, "and director:", director, "added\n")
+	log.Print("HTMX request received")
+	log.Print(r.Header.Get("HX-request"))
+}
+
+func main() {
+	http.HandleFunc("/", HandlerOne)
+	http.HandleFunc("/add-film/", HandlerTwo)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
